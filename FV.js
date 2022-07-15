5a1f531df5360aba2b8b7a55e26d923c24ecdcb9
Matt2017 <mjordan1616@gmail.com>
2022-07-15 01:50:24 -0400

    Add files via upload

---
+++ FV.js
@@ -0,0 +1,22 @@
+let PV;
+let IR;
+let N;
+let FV;
+
+document.getElementById("submitButton").onclick = function(){
+
+    PV = document.getElementById("PVTextBox").value;
+    PV = Number(PV);
+
+    IR = document.getElementById("IRTextBox").value;
+    IR = Number(IR);
+    IR = IR * 0.01;
+
+    N = document.getElementById("YearTextBox").value;
+    N = Number(N);
+
+    //PV = FV/((1+IR)**N);
+    FV = PV*((1+IR)**N);
+
+    document.getElementById("FVLabel").innerHTML = "Future Value: $" + FV;
+}
